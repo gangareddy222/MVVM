3e054a39bec8534936b5b37c1737fe5bcae87889
ANTHIREDDY Gangareddy <gangareddy.anthireddy@hexagon.com>
2025-02-18 17:22:42 +0530

    MVVM COMMIT

---
+++ Models/EmployeeService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvvm.Models
+{
+    public class EmployeeService
+    {
+        private static List<Employee> EmployeeList;
+        public EmployeeService() {
+            EmployeeList = new List<Employee>()
+            {
+              new Employee{ID=101,Name="Ganga",Age=25}
+            };
+        
+        }
+
+        public List<Employee> GetAll()
+        {
+            return EmployeeList;
+        }
+
+        public bool Add(Employee Emp)
+        {
+            if (Emp.Age < 21 || Emp.Age > 58)
+                throw new ArgumentException("Invalid age limit for employee");
+            EmployeeList.Add(Emp);
+            return true;
+
+        }
+
+        public bool Update(Employee EmployeeToUpdate)
+        {
+            bool IsUpdtaed = false;
+            for(int index=0; index < EmployeeList.Count; index++)
+            {
+                if (EmployeeList[index].ID == EmployeeToUpdate.ID)
+                {
+                    EmployeeList[index].Name=EmployeeToUpdate.Name;
+                    EmployeeList[index].Age = EmployeeToUpdate.Age;
+
+                     IsUpdtaed = true;
+                }
+            }
+
+            return IsUpdtaed;
+        }
+
+        public bool Delete(int id)
+        {
+            bool isDeleted = false;
+            for(int i=0; i<EmployeeList.Count; i++)
+            {
+                if (EmployeeList[i].ID == id)
+                {
+                    isDeleted = true;
+                    break;
+                }
+            }
+            return isDeleted;
+        }
+
+        public Employee Search(int id)
+        {
+            return EmployeeList.FirstOrDefault(e => e.ID == id);
+        }
+    }
+}
---
+++ ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Mvvm.Models;
+
+namespace Mvvm.ViewModels
+{
+   public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));  
+            }
+        }
+
+        EmployeeService Obj;
+        public EmployeeViewModel()
+        {
+          Obj =new EmployeeService();
+            
+        }
+        private List<Employee> employeesList;
+        public List<Employee> EmployeesList
+        {
+            get { return employeesList; }
+            set { employeesList = value;OnPropertyChanged("EmployeesList"); }
+        }
+
+    }
+}
